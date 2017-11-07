cdf69fafdb45b0cce1ddd5c946cfa30d95d6070a
febuniac <febuniac@gmail.com>
2017-11-06 23:57:59 -0200

    Subindo dados

---
+++ Arquivo de gerar e jogar para S3/gera_joga.py
@@ -0,0 +1,38 @@
+import boto3
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+import json
+
+ts = TimeSeries(key='URYRAQZOCM9WBDO6', output_format='json')
+
+
+stocks=['AAPL','AXP','BA','CAT','CSCO','CVX','KO',
+        'XOM','GE','GS','HD','IBM','INTC','JNJ',
+        'JPM','MCD','MMM','MRK','MSFT','NKE','PFE','PG',
+        'TRV','UNH','UTX','GOOG','ORCL','BB','AMZN','FB']
+
+for i in stocks:
+    data, meta_data = ts.get_intraday(symbol=i,interval='1min', outputsize='full')
+    print(i,":", data, "\n")
+    with open('json/'+i+'.json', 'w') as txtfile:
+        json.dump(data, txtfile)
+
+# Let's use Amazon S3/ Create an S3 client
+s3 = boto3.resource('s3')
+s3_client = boto3.client('s3')
+
+#bucket = s3.Bucket('aula1ayres')
+filename = 'json/teste.json'
+
+bucket_name = 'testeproj'
+# Print out bucket names
+for bucket in s3.buckets.all():
+    print(bucket.name)
+
+# Uploads the given file using a managed uploader, which will split up large
+# files automatically and upload parts in parallel.
+for i in stocks:
+    filename = 'json/'+i+'.json'
+    s3_client.upload_file(filename, bucket_name, i+'json')
+print("upload done")
+
